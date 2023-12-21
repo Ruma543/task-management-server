6a8d5727dc865fcfe5b480d446a70d9127541a3a
Ruma543 <ruma.afrin543@gmail.com>
2023-12-21 18:25:22 +0600

    crud operation are implemented

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.port || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.TASK_MANAGEMENT_DB}:${process.env.TASK_MANAGEMENT_PASS}@cluster0.hybcmzi.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,10 +27,34 @@ const client = new MongoClient(uri, {
 const taskCollection = client.db('task-managementDB').collection('tasks');
 const userCollection = client.db('task-managementDB').collection('users');
 
+const verifyToken = (req, res, next) => {
+  console.log('verify token', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+    req.decoded = decoded;
+    console.log('checked', req.decoded);
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
+        expiresIn: '1hr',
+      });
+      res.send({ token });
+    });
 
     app.post('/users', async (req, res) => {
       const userInfo = req.body;
@@ -37,6 +67,25 @@ async function run() {
         });
       }
       const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    app.post('/tasks', async (req, res) => {
+      const taskData = req.body;
+      const result = await taskCollection.insertOne(taskData);
+      res.send(result);
+    });
+
+    app.get('/tasks/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await taskCollection.find({ email }).toArray();
+      res.send(result);
+    });
+
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
 
